00cdf48e3455f02305edb192810404cec7d40db1
WayyzOFC <95708143+WayyzOFC@users.noreply.github.com>
2021-12-12 00:31:44 -0300

    Update Warning Note.lua

--- example_mods/custom_notetypes/Warning Note.lua
+++ example_mods/custom_notetypes/Warning Note.lua
@@ -2,26 +2,26 @@ function onCreate()
 	--Iterate over all notes
 	for i = 0, getProperty('unspawnNotes.length')-1 do
 		--Check if the note is an desviation note
-		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'seta cuidado se n morre kkk' then
-			setPropertyFromGroup('unspawnNotes', i, 'texture', 'Seta de alerta'); --Change texture
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Warning Note' then
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'HURTNOTE_assets'); --Change texture
 				setPropertyFromGroup('unspawnNotes', i, 'ignoreNote', false); --Miss has no penalties
 		end
 	end
 	--debugPrint('Script started!')
 	function noteMiss(id, i, noteType, isSustainNote)
-		if noteType == 'seta cuidado se n morre kkk' then
-			playSound('tiro fodao', 1)
+		if noteType == 'Warning Note' then
+			playSound('missnote1', 1)
 			setProperty('health', -500);
 		characterPlayAnim('boyfriend', 'hurt', true);
-		characterPlayAnim('pico', 'down', true);
+		characterPlayAnim('pico', 'idle', true);
 
 	end
 end
 end
 
 function goodNoteHit(id, noteData, noteType, isSustainNote)
-	if noteType == 'seta cuidado se n morre kkk' then
-		playSound('tiro fodao', 1)
+	if noteType == 'Warning Note' then
+		playSound('missnote1', 1)
 		characterPlayAnim('boyfriend', 'dodge', true);
 		characterPlayAnim('pico', 'down', true);
 	end
